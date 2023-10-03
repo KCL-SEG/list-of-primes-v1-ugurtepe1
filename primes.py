721834b55c16f1071492f919a631ff4c9f25cfcf
Ugur Tepe'git config --global user.name Ugur <ugur.tepe@kcl.ac.uk>
2023-10-03 11:54:54 +0100

    list exercise completed

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+    number = 1
+    while len(list) < number_of_primes:
+        if(isPrime(number)):
+            list.append(number)
+        number += 1
     return list
+
+def isPrime(number):
+    if (number == 1 or number == 0):
+        return False
+    if (number == 2):
+        return True
+    for i in range(2,number):
+        if(number % i == 0):
+            return False
+    return True
+
+
+print(primes(12))
